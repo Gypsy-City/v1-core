edee2c6ef21f566b5089cfe18acd1565b8f09088
Bryan Kyritz <bryan@securemeeting.org>
2023-01-09 11:59:39 -0500

    add gas report

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,22 @@
 require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
-require("@nomiclabs/hardhat-truffle5");
-require("solidity-coverage");
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-truffle5"); //allows truffle tests to run in hardhat
+require("solidity-coverage"); //calculates code coverage to ensure better testing
+require("hardhat-gas-reporter"); //calculates gas costs
+
 module.exports = {
   solidity: "0.8.17",
   etherscan: {
     // Your API key for Etherscan
     // Obtain one at https://etherscan.io/
     apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    token: "MATIC", //Can be "ETH" or "MATIC"
+    gasPriceApi:
+      "https://api.polygonscan.com/api?module=proxy&action=eth_gasPrice", //Can be "https://api.etherscan.io/api?module=proxy&action=eth_gasPrice" or "https://api.polygonscan.com/api?module=proxy&action=eth_gasPrice"
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
   },
 };

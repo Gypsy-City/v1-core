1e9ec4ad125b73efacb5cb6970d72a705ddc3a14
Bryan Kyritz <bryan@securemeeting.org>
2023-01-14 19:32:20 -0500

    added deployment scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,38 @@ require("@nomiclabs/hardhat-truffle5"); //allows truffle tests to run in hardhat
 require("@nomiclabs/hardhat-ethers"); //allows injected ethers
 require("solidity-coverage"); //calculates code coverage to ensure better testing
 require("hardhat-gas-reporter"); //calculates gas costs
+require("hardhat-deploy"); //helps with deployments
+require("hardhat-deploy-ethers"); //helps with deployments
+
+const { DEPLOYER_PK, OPERATIONS_PK, GYPSY_PK } = process.env;
 
 module.exports = {
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: false,
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`0x${DEPLOYER_PK}`, `0x${OPERATIONS_PK}`, `0x${GYPSY_PK}`],
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`0x${DEPLOYER_PK}`, `0x${OPERATIONS_PK}`, `0x${GYPSY_PK}`],
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`0x${DEPLOYER_PK}`, `0x${OPERATIONS_PK}`, `0x${GYPSY_PK}`],
+    },
+    polygon: {
+      url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`0x${DEPLOYER_PK}`, `0x${OPERATIONS_PK}`, `0x${GYPSY_PK}`],
+    },
+  },
+  namedAccounts: {
+    deployer: 0,
+    operation_wallet: 1,
+    profit_wallet: 2,
+  },
   solidity: {
     version: "0.8.17",
     settings: {
@@ -21,10 +51,12 @@ module.exports = {
       },
     },
   },
-  etherscan: {
-    // Your API key for Etherscan
-    // Obtain one at https://etherscan.io/
-    apiKey: process.env.ETHERSCAN_API_KEY,
+  verify: {
+    etherscan: {
+      // Your API key for Etherscan
+      // Obtain one at https://etherscan.io/
+      apiKey: process.env.ETHERSCAN_API_KEY,
+    },
   },
   gasReporter: {
     enabled: true,
